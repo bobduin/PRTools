6349df21ca1a31c6f6f2a1631f245df758c00d62
bobduin <bduin@tudelft.net>
2019-05-27 15:19:06 +0200

    all files added

---
+++ @prdatafile/setraw.m
@@ -0,0 +1,12 @@
+%SETTYPE Set TYPE datafile field
+
+% Copyright: R.P.W. Duin, r.p.w.duin@37steps.com
+% Faculty EWI, Delft University of Technology
+% P.O. Box 5031, 2600 GA Delft, The Netherlands
+
+function a = settype(a,s)
+
+	  
+  a.type = s;
+  
+ return
